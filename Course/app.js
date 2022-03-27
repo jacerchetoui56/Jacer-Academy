027cdc5f3a9db9cd27376fd3db7b50e4b78c802c
jacerchetoui56 <jacerdjo@gmail.com>
2022-03-27 20:43:03 +0100

    a good first step to making it pretty solid

--- Course/app.js
+++ Course/app.js
@@ -1,8 +1,5 @@
-const dark = document.querySelector(".darkmodetoggle")
 const body = document.querySelector("body")
 const sell = document.querySelector("#sell")
-const Dark = document.querySelector(".fa-moon")
-const light = document.querySelector(".fa-sun")
 const toplogolight = document.querySelector(".lightlogo")
 const toplogodark = document.querySelector(".darklogo")
 const scrolltotop = document.querySelector(".scrolltop")
@@ -16,15 +13,7 @@ burger.addEventListener('click',()=>{
 
 })
 
-dark.addEventListener('click',()=>{
-    body.classList.toggle("dark")
-    sell.classList.toggle("dark")
-    dark.classList.toggle("dark")
-    toplogodark.classList.toggle("darkmode")
-    toplogolight.classList.toggle("darkmode")
-    scrolltotop.classList.toggle("dark")
-    home.classList.toggle("dark")
-})
+
 
 window.onscroll = () =>{
     if(document.documentElement.scrollTop > 900) scrolltotop.classList.add("show")
