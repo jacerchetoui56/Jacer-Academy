17e24a0e8bf203145a2b2ce399ecfe29852b4f76
jacerchetoui56 <jacerdjo@gmail.com>
2022-04-08 04:47:17 +0100

    almost finished the html course (it's pretty gorgeous) and I started the following left menu to scroll between the sections

--- course-page/main.js
+++ course-page/main.js
@@ -3,12 +3,12 @@ const scrolltotop = document.querySelector(".scrolltop")
 window.onscroll = () =>{
     if(window.scrollY >= 1500) scrolltotop.classList.add("show")
     else scrolltotop.classList.remove("show")
-    if(window.scrollY>=0 && window.scrollY<590){
+    if(window.scrollY>=0 && window.scrollY<900){
         indexes[0].classList.add("selected")
         indexes[1].classList.remove("selected")
         indexes[2].classList.remove("selected")
     }
-    else if(window.scrollY>=590 && window.scrollY<1350){
+    else if(window.scrollY>=900 && window.scrollY<1350){
         indexes[0].classList.remove("selected")
         indexes[1].classList.add("selected")
         indexes[2].classList.remove("selected")
